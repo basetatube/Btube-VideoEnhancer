dee64acbe512482bf6144f3e8591334cbdda849b
Baseta Tube <134952017+basetatube@users.noreply.github.com>
2023-09-29 20:14:39 +0300

    Update about.py

--- facefusion/uis/components/about.py
+++ facefusion/uis/components/about.py
@@ -9,4 +9,4 @@ ABOUT_HTML : Optional[gradio.HTML] = None
 def render() -> None:
 	global ABOUT_HTML
 
-	ABOUT_HTML = gradio.HTML('<center><b><a href='https://www.youtube.com/@basetatube/'>Baseta Tube</a></b> Video Enhancer</br> ported from </br><a href="' + metadata.get('url') + '">' + metadata.get('name') + ' ' + metadata.get('version') + '</a></center>')
+	ABOUT_HTML = gradio.HTML('<center><b><a href=https://www.youtube.com/@basetatube/>Baseta Tube</a></b> Video Enhancer</br> ported from </br><a href="' + metadata.get('url') + '">' + metadata.get('name') + ' ' + metadata.get('version') + '</a></center>')
